0412de9d59359071ca491fc2b982c91d46230bba
mikesamoylove <mikesamoylove@yandex.ru>
2026-01-05 20:43:59 +0300

    add 4-expenses project

--- 1-vars/hello.py
+++ 1-vars/hello.py
@@ -1,3 +1,11 @@
-name = input("Введите ваше имя: ")
-age = input("Введите ваш возрат: ")
-print(f"Привет, {name}! Тебе {age} лет")
+first_list = [1, 2, 3]
+second_list = [4, 5, 6]
+
+# Ваш код здесь
+print(f"Первый список: {first_list}")
+print(f"Второй список: {second_list}")
+united_list = first_list + second_list
+print(f"Объединенный список: {united_list}")
+print(f"Длина объединенного списка: {len(united_list)}")
+print(f"Первый элемент: {united_list[0]}")
+print(f"Последний элемент: {united_list[-1]}")
