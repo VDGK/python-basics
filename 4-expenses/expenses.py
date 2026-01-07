8b301f209f52a1ad7cb82797c823503e870ea04c
mikesamoylove <mikesamoylove@yandex.ru>
2026-01-07 16:10:17 +0300

    fix issues

--- 4-expenses/expenses.py
+++ 4-expenses/expenses.py
@@ -1,7 +1,7 @@
 expenses = input("Введите сумму формата \"<руб> руб <коп> коп \": ").strip().lower().split(" ")
-if len(expenses) < 3:
+if len(expenses) < 3 and expenses[1] == "руб":
     print(f"{int(expenses[0]):.2f} ₽")
-elif len(expenses) == 4:
-    print(f"{int(expenses[0])}.{int(expenses[2])} ₽")
+elif len(expenses) == 4 and expenses[1] == "руб" and expenses[3] == "коп":
+    print(f"{int(expenses[0])}.{int(expenses[2]):.2f} ₽")
 else:
     print("Некорректный формат суммы")
