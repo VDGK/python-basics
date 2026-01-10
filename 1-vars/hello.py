8d60f40aeb259f23b988fd9b64b7aa04ef1c93d6
mikesamoylove <mikesamoylove@yandex.ru>
2026-01-10 21:35:41 +0300

    base scrips

--- 1-vars/hello.py
+++ 1-vars/hello.py
@@ -1,5 +1,7 @@
-full_name = "Мария Иванова"
-
+numbers = [1, 2, 3, 2, 4, 1, 5]
+unique_elements = []
+for number in numbers:
+  if number not in unique_elements:
+    unique_elements.append(number)
 # Ваш код здесь
-name, surname = full_name.split(" ")
-print(f"Имя: {name}\nФамилия: {surname}")
+print(unique_elements)
